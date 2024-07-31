70118cdabc9926c0da4b878d9a3236d45c50c033
Aarjav Jain <anshjain638@gmail.com>
2024-07-31 21:09:44 +0530

    added spash screen

--- Client/PostHub/screens/splash.js
+++ Client/PostHub/screens/splash.js
@@ -1,11 +1,24 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, SafeAreaView} from 'react-native';
 import React from 'react';
 
 const Splash = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/Icon.png')} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.IconWrapper}>
+        <Image
+          source={require('../assets/Icon.png')}
+          style={{height: 80, width: 80, objectFit: 'contain'}}
+        />
+        <Text style={{fontSize: 22, color: 'gray', textTransform: 'uppercase'}}>
+          PostHub
+        </Text>
+      </View>
+
+      <View style={styles.TextWrapper}>
+        <Text style={{fontSize: 18, color: 'gray'}}>From</Text>
+        <Text style={{fontSize: 25, color: 'gray'}}>Aarjav</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,8 +26,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e1d5c9',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  IconWrapper: {
+    flex: 4,
+    gap: 10,
+    marginVertical: '2%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  TextWrapper: {
+    flex: 1,
+    marginVertical: '2%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
   },
 });
 
